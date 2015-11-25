6060008270a8bdc6f076045511ccdbdd94f894a5
Dario Chamorro <dario.chamorro.v@gmail.com>
2015-11-25 18:12:49 -0500

    Finalizacion de Proyecto

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var num1: UITextField!
+    @IBOutlet var num2: UITextField!
+    @IBOutlet var rta: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sumar(sender: UIButton) {
+        let n1:Int = Int(num1.text!)!
+        let n2:Int = Int(num2.text!)!
+        
+        let r:Int =  n1+n2
+        
+        rta.text = "\(r)"
+        
+        
+    }
 
+    @IBAction func restar(sender: AnyObject) {
+        let n1:Int = Int(num1.text!)!
+        let n2:Int = Int(num2.text!)!
+        
+        let r:Int =  n1-n2
+        
+        rta.text = "\(r)"
+    }
+    
+    @IBAction func multiplicar(sender: AnyObject) {
+        let n1:Int = Int(num1.text!)!
+        let n2:Int = Int(num2.text!)!
+        
+        let r:Int =  n1*n2
+        
+        rta.text = "\(r)"
+    }
+    
+    @IBAction func dividir(sender: AnyObject) {
+        let n1:Int = Int(num1.text!)!
+        let n2:Int = Int(num2.text!)!
+        
+        let r:Int =  n1/n2
+        
+        rta.text = "\(r)"
+    }
 }
 
